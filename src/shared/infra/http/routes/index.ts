9fcc9e2e3423c4ca532bcdbe5da55b6e2b5f5243
João Castelli <joaao.castelli@gmail.com>
2020-05-24 10:13:26 -0300

    implemented validations

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,7 +4,7 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
-import providersRouter from '@modules/appointments/infra/http/routes/providers.routes copy';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 
 const routes = Router();
 
