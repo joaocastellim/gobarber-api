e7624fffa70ae95b8b10d554f57b215ce60235bb
João Castelli <joaao.castelli@gmail.com>
2020-05-24 10:39:00 -0300

    implemented class-transformer

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatartService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -11,7 +12,6 @@ export default class UserAvatarController {
       avatarFilename: request.file.filename,
     });
 
-    delete user.password;
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
