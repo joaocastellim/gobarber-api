d3188f2063143f3521117443e387ef15cd3e6e57
João Castelli <joaao.castelli@gmail.com>
2020-05-28 15:00:16 -0300

    implemented cache

--- src/modules/appointments/services/ListProvidersService.spec.ts
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,14 +1,20 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import ListProvidersSerivce from './ListProvidersService';
 
 let fakeUsersRepository: FakeUsersRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let listProviders: ListProvidersSerivce;
 
 describe('ListProviders', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
+    fakeCacheProvider = new FakeCacheProvider();
 
-    listProviders = new ListProvidersSerivce(fakeUsersRepository);
+    listProviders = new ListProvidersSerivce(
+      fakeUsersRepository,
+      fakeCacheProvider,
+    );
   });
 
   it('should be able to list of providers', async () => {
